da402a94d25b3979a8f19c3c551ec4c3932a13a9
showk mahmoud <showkmahmoud@gmail.com>
2024-10-11 14:55:55 +0400

    NF: job listings

--- src/components/JobListings/JobListings.jsx
+++ src/components/JobListings/JobListings.jsx
@@ -1,4 +1,6 @@
+import jobs from "../../data/jobs.json";
 const JobListings = () => {
+  console.log(jobs);
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -6,8 +8,41 @@ const JobListings = () => {
           Browse Jobs
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {/* jobs list */}
+          {jobs.map((job) => (
+            <div
+              key={job.id}
+              className="bg-white rounded-xl shadow-md relative"
+            >
+              <div className="p-4">
+                <div className="mb-6">
+                  <div className="text-gray-600 my-2">{job.type}</div>
+                  <h3 className="text-xl font-bold">{job.title}</h3>
+                </div>
+
+                <div className="mb-5">{job.description}</div>
+
+                <h3 className="text-indigo-500 mb-2">{job.salary}</h3>
+
+                <div className="border border-gray-100 mb-5"></div>
+
+                <div className="flex flex-col lg:flex-row justify-between mb-4">
+                  <div className="text-orange-700 mb-3">
+                    <i className="fa-solid fa-location-dot text-lg"></i>
+                    {job.location}
+                  </div>
+                  <a
+                    href={`/job/${job.id}`}
+                    className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+                  >
+                    Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
           {/* <!-- Job Listing 1 --> */}
-          <div className="bg-white rounded-xl shadow-md relative">
+          {/* <div className="bg-white rounded-xl shadow-md relative">
             <div className="p-4">
               <div className="mb-6">
                 <div className="text-gray-600 my-2">Full-Time</div>
@@ -37,9 +72,9 @@ const JobListings = () => {
                 </a>
               </div>
             </div>
-          </div>
+          </div> */}
           {/* <!-- Job Listing 2 --> */}
-          <div className="bg-white rounded-xl shadow-md relative">
+          {/* <div className="bg-white rounded-xl shadow-md relative">
             <div className="p-4">
               <div className="mb-6">
                 <div className="text-gray-600 my-2">Remote</div>
@@ -70,9 +105,9 @@ const JobListings = () => {
                 </a>
               </div>
             </div>
-          </div>
+          </div> */}
           {/* <!-- Job Listing 3 --> */}
-          <div className="bg-white rounded-xl shadow-md relative">
+          {/* <div className="bg-white rounded-xl shadow-md relative">
             <div className="p-4">
               <div className="mb-6">
                 <div className="text-gray-600 my-2">Remote</div>
@@ -102,7 +137,7 @@ const JobListings = () => {
                 </a>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
