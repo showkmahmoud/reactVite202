de4c88a0a300ceeba88ce412225d2d238d5ae157
showk mahmoud <showkmahmoud@gmail.com>
2024-10-11 14:32:27 +0400

    NF: adding card UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Hero from "./components/Hero/Hero";
+import HomeCards from "./components/HomeCards/HomeCards";
 import NavBar from "./core/Navbar/NavBar";
 
 const App = () => {
   return (
     <>
       <NavBar />
+      <Hero />
+      <HomeCards />
     </>
   );
 };
