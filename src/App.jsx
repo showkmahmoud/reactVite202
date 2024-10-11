d94a901713ee10198c592dbac01dadd7ea395741
showk mahmoud <showkmahmoud@gmail.com>
2024-10-11 14:43:06 +0400

    NF: adding the jobs.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Hero from "./components/Hero/Hero";
 import HomeCards from "./components/HomeCards/HomeCards";
+import JobListings from "./components/jobListings/jobListings";
 import NavBar from "./core/Navbar/NavBar";
-
 const App = () => {
   return (
     <>
       <NavBar />
       <Hero />
       <HomeCards />
+      <JobListings />
     </>
   );
 };
